35b804ae175b3276cb979bd8c84ed9954d0492a2
Ryan D. Lewis <ryan.lewis@anl.gov>
2024-03-05 16:49:45 -0600

    Updating to use new OT2 module

--- x4_app/x4_app.py
+++ x4_app/x4_app.py
@@ -9,26 +9,25 @@ from wei import ExperimentClient
 
 def main() -> None:
     """Runs an example WEI workflow"""
-    # The path to the Workflow definition yaml file
-    wf_path = Path(__file__).parent / "workflows" / "example_workflow.yaml"
-
     # This defines the Experiment object that will communicate with the WEI server
-    exp = ExperimentClient("localhost", "8000", "Example_Program")
+    exp = ExperimentClient("localhost", "8000", "X4_Experiment")
+
+    # The path to the Workflow definition yaml file
+    wf_path = Path(__file__).parent / "workflows" / "x4_run_ot2_protocol.yaml"
 
     # This runs the workflow
     flow_info = exp.start_run(
         wf_path.resolve(),
         payload={
-            "wait_time": 5,
-            "file_name": "experiment_output.jpg",
+            "protocol_file": "path/to/protocol/file"
         },
     )
     print(json.dumps(flow_info, indent=2))
 
     # The below line can be used to fetch the result and save it in our local directory
     exp.get_file(
-        flow_info["hist"]["Take Picture"]["action_msg"],
-        "/home/app/.wei/experiment_output.jpg",
+        flow_info["hist"]["Run OT2 Protocol"]["action_msg"],
+        "./protocol_run_log.json",
     )
 
 
