3c4d5c34816bb3650cbd3e7ac12134e5f1fcc553
Ryan Lewis <ryan.lewis@anl.gov>
2024-06-27 09:47:37 -0500

    Multi-plate reader tests

--- x4_app/x4_app.py
+++ x4_app/x4_app.py
@@ -11,11 +11,11 @@ from wei import ExperimentClient
 def main() -> None:
     """Runs an example WEI workflow"""
     # This defines the Experiment object that will communicate with the WEI server
-    exp = ExperimentClient("localhost", "8000", "X4_Experiment")
+    exp = ExperimentClient("localhost", "8000", "X4_Experiment", email_addresses=["ryan.lewis@anl.gov", "Shkrob@anl.gov"])
 
     # The path to the Workflow definition yaml file
-    wf_path = Path(__file__).parent / "workflows" / "x4_test_epoch2.yaml"
-
+    wf_pr1 = Path(__file__).parent / "workflows" / "x4_test_platereader1.yaml"
+    wf_pr2 = Path(__file__).parent / "workflows" / "x4_test_platereader2.yaml"
 
     # init_resources={}
     # resources = state_of_resources_now()
@@ -29,16 +29,23 @@ def main() -> None:
 
     # current_protocol = create_current_protocol(resources)
 
-    gen5xpt = "C:\\Users\\Public\\Documents\\Plate Reader\\DATA\\Gen5 w WEI\\"
-    gen5xpt += "6-20-2024 16 reagents\\B21_plate_read_all384wells.xpt"
+    gen5xpt_base = "C:\\Users\\Public\\Documents\\Plate Reader\\DATA\\Gen5 w WEI\\"
+    gen5xpt_pr1 = gen5xpt_base + "6-20-2024 16 reagents\\pr1_B21_plate_read_all384wells.xpt"
+    gen5xpt_pr2 = gen5xpt_base + "6-20-2024 16 reagents\\pr2_dummy_plate_read_all384wells.xpt"
 
     # This runs the workflow
     for i in range(1000):
-        flow_info = exp.start_run(
-            wf_path.resolve(),
-            payload={"experiment_file_path" : gen5xpt},
+        flow_info_1 = exp.start_run(
+            wf_pr1.resolve(),
+            payload={"experiment_file_path" : gen5xpt_pr1},
+            blocking=False
         )
-        print(json.dumps(flow_info, indent=2))
+        flow_info_2 = exp.start_run(
+            wf_pr2.resolve(),
+            payload={"experiment_file_path" : gen5xpt_pr2},
+            blocking=True
+        )
+        print(json.dumps(flow_info_2, indent=2))
 
     # The below line can be used to fetch the result and save it in our local directory
     # exp.get_file(
